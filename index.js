b7466c1da7e70e3e1702fd773a6368560eac0b52
Erica Hashert <ericahashert@gmail.com>
2022-08-15 15:38:12 -0600

    commit tree 3c5e84720e0978b9d5e3d2ee40f31fabb075d44d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
